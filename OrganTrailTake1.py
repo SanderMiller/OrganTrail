2fc82ee12bbb35d96cf408177843855d6a35397b
SanderMiller <42980999+SanderMiller@users.noreply.github.com>
2019-12-16 19:14:42 -0500

    Add files via upload

--- OrganTrailTake1.py
+++ OrganTrailTake1.py
@@ -1,40 +1,25 @@
 import random
-def printResponse():
-    attributes = []
-    Input = input()
-    print("You Chose: " + response[0][int(Input)-1])
-    if Input == '1':
-        health = 15
-        strength = 90
-        merit = 0
-    elif Input == '2':
-        health = 10
-        strength = 75
-        merit = 0
-    elif Input == '3':
-        health = 5
-        strength = 50
-        merit = 0
-    attributes.append(health)
-    attributes.append(strength)
-    attributes.append(merit)
-    print('Your attributes are: Health = ' + str(attributes[0]) + ' Strength = ' + str(attributes[1]))
-    return attributes
-
-
-
 
 currentScenario = 'Scenario1'
 
 dialogue = {
-    'Scenario1': ['Choose your character', ['Pharaoh (Easy)', 'Priest (Normal)', 'Farmer (Hard)'], ['Scenario2', 'Scenario2', 'Scenario2']],
-    'Scenario2': ['Anubis asks you to follow him. What will you do?', ['follow him', 'stay where you are'], ['Scenario3', 'ScenarioEnd']],
-    'Scenario3': ['You follow him into a crowd, but soon you lose sight of him... you look around, there is a traveler standing by themselves at the wall, what would you like to do',
-                 ['Approach them and ask for directions', 'Fight them for food (or bragging rights)', 'make your way on your own'], ['Scenario4', 'ScenarioFight', 'ScenarioEnd']],
+    'Scenario1': ['Choose your character', 
+                 ['Pharaoh', 'Priest', 'Farmer'], ['Scenario2', 'Scenario2', 'Scenario2'], [[15,50,0,3], [10,60,1,5], [15,70,0,7]]],
+    'Scenario2': ['We must make haste to the hall of two truths, so that your soul can be weighed against a feather and we can learn whether you may pass into the afterlife. Any questions? \n “W- w-” \n “Good! Let’s get on with it then.” Anubis rushes out of the room with no further words, what will you do?.', 
+                 ['follow him', 'ignore him, and stay where you are'], ['Scenario3', 'ScenarioEnd'], [[-1,0,0,3], [-1,0,0,5], [-1,0,0,7]]],
+    'Scenario3': ['You follow him into a crowd, but soon you lose sight of him... you look around, there is a traveler standing by themselves at the wall, what would you like to do?',
+                 ['Approach them and ask for directions', 'Fight them for food (or bragging rights)', 'make your way on your own'], ['Scenario4', 'ScenarioFight', 'Scenario6'], [[-1,0,0,3], [-1,0,0,5], [-1,0,0,7]]],
     'Scenario4': ['You approach the stranger. She turns around and you notice her appearance. She had the body of a leopard, but her head was that of a hippopotamus. She held a "Khopesh", a massive sword and sickle hybrid. When she spoke, her voice was powerful and intimidating."Hello weary traveler, I am Henet Requ, guardian of the portal of fire. You look lost, would you like my assistance?"',
-                 ['Accept her offer', 'Make your way on your own', 'challenge her to a dance off'], ['Scenario5', 'Scenario6', 'ScenarioEnd']],
-    'Scenario5': ['"Hello Henet, I was following Anubis but seem to have lost my way, do you know the way to the Hall of Maat?" Her brow furrowed. I do know the way, although it is very treacherous. You must follow me.', ['Follow her', 'come on seriously, follow her', 'why are you not trusting Henet'], ['ScenarioEnd', 'ScenarioEnd', 'ScenarioEnd']],
-    'Scenario6': ['You try to make it through crowd on your own. Eventually after fighting the current of souls you see a gate . There appear to be two options, a water and land route. What do you do?', ['Approach the land route gate.','Approach the water route'], ['ScenarioEnd', 'ScenarioEnd']],
+                 ['Accept her offer', 'Make your way on your own', 'challenge her to a dance off'], ['Scenario5', 'Scenario6', 'ScenarioEnd'], [[-1,0,0,3], [-1,0,0,5], [-1,0,0,7]]],
+    'Scenario5': ['"Hello Henet, I was following Anubis but seem to have lost my way, do you know the way to the Hall of Maat?" Her brow furrowed. I do know the way, although it is very treacherous. You must follow me.', 
+                 ['Follow her', 'come on seriously, follow her', 'why are you not trusting Henet'], ['ScenarioEnd', 'ScenarioEnd', 'ScenarioEnd'], [[-1,0,0,3], [-1,0,0,5], [-1,0,0,7]]],
+    'Scenario6': ['You try to make it through crowd on your own. Eventually after fighting the current of souls you see a gate . There appear to be two options, a water and land route. What do you do?', 
+                 ['Approach the land route gate.','Approach the water route'], ['ScenarioEnd', 'ScenarioEnd'], [[0,0,0,3], [0,0,0,5], [0,0,0,7]]], 
+    #'Scenario7': ['',[],[]]
+    #'Scenario8': ['',[],[]]
+    #'Scenario9': ['',[],[]]
+    #'Scenario10': ['',[],[]]
+    #'Scenario11': ['',[],[]]
     'ScenarioEnd': ['Alas, you perish, never to set eyes on the Hall of Maat. Thanks for playing!', ['you lose', 'you lose (even more horribly)'], ['Scenario1', 'Scenario1']]
 }
 
@@ -46,7 +31,8 @@ def fight(strength, health, currentScenario, lastScenario):
     if outcome <= strength:
         health += 3
         currentScenario = lastScenario
-        print('Congrats, you won!\n')
+        print('Congrats, you defeated a demon!\n')
+        print('Health' + str(health))
     else:
         currentScenario = 'ScenarioEnd'
         print('Sorry, you lost.\n')
@@ -55,10 +41,14 @@ def fight(strength, health, currentScenario, lastScenario):
 
 
 def gameplay(currentScenario):
-    print('What is your name?')
+    print('Your eyes open, or at least so it seems, but they’re met with a disconcerting sight. A coffin lies beneath you, with inscriptions that match your name. You notice a strange figure in the corner of the room. As they approach, you recognize it to be Anubis, the jackal headed god of the dead, son of Osiris. Anubis asks,“What is your name?” ')
     name = input()
-    print("Hello "+name)
+    print("Hello " + name+", you must be very confused. I’m afraid to say that you’re dead, due to a terrible accident. Don’t panic, death gets a bad reputation but it’s alright. We should get going, but real quick, what was your occupation?")
     scenarioCount = 0
+    attributes = {'Health': 0,
+                  'Strength': 0,
+                  'Merit': 0,
+                  'TurnPerFood': 0 }
     while 1:
 
         if currentScenario == 'ScenarioFight':
@@ -75,36 +65,27 @@ def gameplay(currentScenario):
             print(responseString2)
 
         Input = input()
-        if currentScenario == 'Scenario1':
-            attributes = []
 
-            if Input == '1':
-                health = 15
-                strength = 90
-                merit = 0
-                characterFood = 3
-            elif Input == '2':
-                health = 10
-                strength = 75
-                merit = 0
-                characterFood = 5
-            elif Input == '3':
-                health = 5
-                strength = 50
-                merit = 0
-                characterFood = 7
-            attributes.append(health)
-            attributes.append(strength)
-            attributes.append(merit)
-            print('Your attributes are: Health = ' + str(attributes[0]) + ' Strength = ' + str(attributes[1])+'\n')
+       
+        attributes['Health'] += dialogue[currentScenario][3][int(Input) - 1][0]
+        attributes['Strength'] += dialogue[currentScenario][3][int(Input) - 1][1]
+        attributes['Merit'] += dialogue[currentScenario][3][int(Input) - 1][2]
+        attributes['TurnPerFood'] += dialogue[currentScenario][3][int(Input) - 1][3]
+        health = attributes['Health']
+        TurnPerFood = attributes['TurnPerFood']
+           
+          
+        #print('Your attributes are: Health = ' + str(attributes[0]-1) + ' Strength = ' + str(attributes[1])+'\n')
+        
 
         lastScenario = currentScenario
         currentScenario = dialogue[currentScenario][2][int(Input) - 1]
         health -=1
         scenarioCount+=1
-        if scenarioCount % characterFood == 0:
+        if scenarioCount % TurnPerFood == 0:
             health+=2
             print('Your tomb recieved an offering, your health has increased by 2')
+        print('Health:' + str(health))
 
     # try:
     #     printResponse()
