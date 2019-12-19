7772d25ad84f438b7ceff5e86f8a626dbeb181ce
SanderMiller <amiller@olin.edu>
2019-12-19 14:47:29 -0500

    Added Fight and scenario 7

--- PygameVisual.py
+++ PygameVisual.py
@@ -20,27 +20,31 @@ dialogue = {
                  ['Follow him', 'Ignore him, and stay where you are'], ['Scenario3', 'ScenarioEnd'], [[-1,0,0], [-1,0,0], [-1,0,0]], ['PyramidBackground.jpg', 'Anubis.png']],
     'Scenario3': ['You follow him into a crowd, but soon you lose sight of him... you look around, \
                   \nthere is a traveler standing by themselves at the wall, what would you like to do?',
-                 ['Approach them and ask for directions', 'Fight them for food (or bragging rights)', 'make your way on your own'], ['Scenario4', 'ScenarioFight', 'Scenario6'], [[-1,0,0], [-1,0,0], [-1,0,0]],['Cavern.jpg']],
+                 ['Approach them and ask for directions', 'Fight them for food (or bragging rights)', 'Make your way on your own'], ['Scenario4', 'ScenarioFight', 'Scenario6', 'Scenario6'], [[-1,0,0], [-1,0,0], [-1,0,0]],['Cavern.jpg']],
     'Scenario4': ['You approach the stranger. She turns around and you notice her appearance. \
                   \nShe had the body of a leopard, but her head was that of a hippopotamus. She held a "Khopesh", \
                   \na massive sword and sickle hybrid. When she spoke, her voice was powerful and intimidating. \
                   \n"Hello weary traveler, I am Henet Requ, guardian of the portal of fire. You look lost, \
                   \nwould you like my assistance?"',
-                 ['Accept her offer', 'Make your way on your own', 'challenge her to a dance off'], ['Scenario5', 'Scenario6', 'ScenarioEnd'], [[-1,0,0], [-1,0,0], [-1,0,0]],['Cavern.jpg', 'Henet.jpg']],
-    'Scenario5': ['"Hello Henet, I was following Anubis but seem to have lost my way, do you know the way \
+                 ['Accept her offer', 'Make your way on your own', 'Challenge her to a dance off'], ['Scenario5', 'Scenario6', 'ScenarioEnd'], [[-1,0,0], [-1,0,0], [-1,0,0]],['Cavern.jpg', 'Henet.jpg']],
+    'Scenario5': ['"Hello Taweret, I was following Anubis but seem to have lost my way, do you know the way \
                   \nto the Hall of Maat?" Her brow furrowed. "I do know the way, although it is very treacherous. \
                   \nYou must follow me."',
-                 ['Follow her', 'come on seriously, follow her', 'why are you not trusting Henet'], ['ScenarioEnd', 'ScenarioEnd', 'ScenarioEnd'], [[-1,0,0], [-1,0,0], [-1,0,0]],['Cavern.jpg', 'Henet.jpg']],
+                 ['Follow her', 'Suspecting a trap, runaway'], ['Scenario7', 'Scenario6'], [[-1,10,0], [-1,0,0], [-1,0,0]],['Cavern.jpg', 'Henet.jpg']],
     'Scenario6': ['You try to make it through crowd on your own. Eventually after fighting the current of \
                   \nsouls you see a gate. There appear to be two options, a water and land route. \
                   \nWhat do you do?',
                  ['Approach the land route gate.','Approach the water route'], ['ScenarioEnd', 'ScenarioEnd'], [[0,0,0], [0,0,0], [0,0,0]],['PyramidBackground.jpg']],
-    #'Scenario7': ['',[],[]]
+    'Scenario7': ['Congrats, through apotropaism you have befriended Taweret, the goddess of childbirth \
+                  \nand fertility. By converting this god to your side, your strength has increased by 10! \
+                  \n\nYou follow Taweret, who seems to easily navigate through the crowd of souls. Not wanting \
+                  \nto lose your guide again you hastily follow. Eventually she leads you to a set of gates,\
+                  \none leads to a vast ocean, while the other leads to stretch of land, full of fire.',['eat', 'eat'],['ScenarioEnd','ScenarioEnd'],[[-1,0,0], [-1,0,0], [-1,0,0]],['PyramidBackground.jpg']],
     #'Scenario8': ['',[],[]]
     #'Scenario9': ['',[],[]]
     #'Scenario10': ['',[],[]]
     #'Scenario11': ['',[],[]]
-    'ScenarioEnd': ['', ['Play Again', 'Play Again'], ['Scenario1', 'Scenario1'],[[0,0,0], [0,0,0], [0,0,0]],['Youlost.jpg']]
+    'ScenarioEnd': ['k', ['Play Again', 'Play Again'], ['Scenario1', 'Scenario1'],[[0,0,0], [0,0,0], [0,0,0]],['Youlost.jpg']]
     }
 
 file_path1 = '/home/cmay/Documents/OrganTrail/'
@@ -59,10 +63,11 @@ def resize(file_name, xDimension, yDimension):
 
 
 def fight(strength, health, currentScenario, lastScenario):
+    global dialogue
     outcome = random.randint(1,100)
     if outcome <= strength:
         health += 3
-        currentScenario = lastScenario
+        currentScenario = dialogue[lastScenario][2][3]
         print('Congrats, you defeated a demon!\n')
         print('Health' + str(health))
     else:
@@ -76,7 +81,7 @@ def multiline_render(text, x, y, font):
     for i,l in enumerate(lines):
         screen.blit(font.render(l, 0, white), (x, y + font.get_linesize()*i))
 
-def visualize(currentScenario, health, strength):
+def visualize(currentScenario, health, strength, additionalString):
     global dialogue
     avatar_status = True
     pygame.display.set_caption('Organ Trail')
@@ -106,6 +111,7 @@ def visualize(currentScenario, health, strength):
     screen.blit(rightKey, [780, 820])
     screen.blit(font.render(healthText, 0, white), [1200, 50])
     screen.blit(font.render(strengthText, 0, white), [1200, 100])
+    multiline_render(additionalString, 300, 50, font)
     multiline_render(option1, 435, 860, font)
     multiline_render(option2, 900, 860, font)
 
@@ -143,7 +149,7 @@ numOptions = len(dialogue[currentScenario][1])
 #name = input()
 
 #print("Hello " + name+", you must be very confused. I’m afraid to say that you’re dead, due to a terrible accident. Don’t panic, death gets a bad reputation but it’s alright. We should get going, but real quick, what was your occupation?")
-visualize(currentScenario, 0, 0)
+visualize(currentScenario, 0, 0, '')
 pygame.display.flip()
 
 
@@ -159,20 +165,7 @@ while dead == False:
         if event.type == pygame.QUIT:
             dead = True
 
-
-
-
-        print('\n' +dialogue[currentScenario][0])
-
         numOptions = len(dialogue[currentScenario][1])
-
-
-        if numOptions == 3:
-            responseString3 = 'Press 1 for ' + dialogue[currentScenario][1][0] + '. Press 2 for ' + dialogue[currentScenario][1][1] + '. Press 3 for ' + dialogue[currentScenario][1][2]+ '.\n'
-            print(responseString3)
-        elif numOptions == 2:
-            responseString2 = 'Press 1 for ' + dialogue[currentScenario][1][0] + '. Press 2 for ' + dialogue[currentScenario][1][1] +'.\n'
-            print(responseString2)
 
         if (event.type == pygame.KEYDOWN and numOptions == 3 and (event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP)) or (event.type == pygame.KEYDOWN and (event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT)):
             print(numOptions)
@@ -202,14 +195,16 @@ while dead == False:
             currentScenario = dialogue[currentScenario][2][int(Input) - 1]
             if currentScenario == 'ScenarioFight':
                 [health, currentScenario] = fight(strength, health, currentScenario, lastScenario)
-            visualize(currentScenario, health, strength)
+            
 
             scenarioCount+=1
             if scenarioCount % TurnPerFood == 0:
                 health+=2
-                print('Your tomb recieved an offering, your health has increased by 2')
-            print('Health:' + str(health))
-
+                additionalString = 'Your tomb recieved an offering, your health has increased by 2!'
+            else:
+                additionalString = ''
+            #print('Health:' + str(health))
+            visualize(currentScenario, health, strength, additionalString)
             pygame.display.flip()
 
             clock.tick(clock_tick_rate)
