839faef6725aa687e0f9d980d4a5a658fac20104
Casey May <cmay@olin.edu>
2019-12-19 22:32:37 -0500

    Adding Fire Route and Most of the Ending Story

--- PygameVisual.py
+++ PygameVisual.py
@@ -22,21 +22,27 @@ dialogue = {
                  ['Follow him', 'Ignore him, and stay where you are'], ['Scenario3', 'ScenarioEnd'], [[-1,0,0], [-1,0,0], [-1,0,0]], ['PyramidBackground.jpg', 'Anubis.png']],
     'Scenario3': ['You follow him into a crowd, but soon you lose sight of him... you look around, \
                   \nthere is a traveler standing by themselves at the wall, what would you like to do?',
-                 ['Approach them and ask for directions', 'Fight them for food (or bragging rights)', 'Make your way on your own'], ['Scenario4', 'ScenarioFight', 'Scenario6', 'Scenario6'], [[-1,0,0], [-1,0,0], [-1,0,0]],['Cavern.jpg']],
-    'Scenario4': ['You approach the stranger. She turns around and you notice her appearance. \
-                  \nShe had the body of a leopard, but her head was that of a hippopotamus. She held a "Khopesh", \
+                 ['Approach them and ask for directions', 'Fight them for food (or bragging rights)', 'Make your way on your own'], ['Scenario5', 'ScenarioFight', 'Scenario6', 'Scenario6'], [[-1,0,0], [-1,0,0], [-1,0,0]],['Cavern.jpg']],
+    'Scenario4': ['You step through the portal and enter a room with fire blazing all around you. \
+                  \nInstantly smoke fills your eyes and heat presses in from all directions.\
+                  \n A figure approaches you, and although your vision is hazy, you pick out some details. \
+                  \nHer body was an amalgam of different animals, with her legs and hands those of a lion,\
+                  \ntail of a crocodile, but her head was that of a hippopotamus. She held a "Khopesh", \
                   \na massive sword and sickle hybrid. When she spoke, her voice was powerful and intimidating. \
-                  \n"Hello weary traveler, I am Henet Requ, guardian of the portal of fire. You look lost, \
-                  \nwould you like my assistance?"',
-                 ['Accept her offer', 'Make your way on your own', 'Challenge her to a dance off'], ['Scenario5', 'Scenario6', 'ScenarioEnd'], [[-1,0,0], [-1,0,0], [-1,0,0]],['Cavern.jpg', 'Henet.jpg']],
-    'Scenario5': ['"Hello Taweret, I was following Anubis but seem to have lost my way, do you know the way \
+                  \n"Hello weary traveler, I am the guardian of the portal of fire. You dare attempt to cross \
+                  \nmy domain unimpeded?" There is no hope of passing by. I do not judge those who come to me \
+                  \non their goodness or badness, only on their quick wit. Leave before I am forced to overpower\
+                  \nyou" You seem to remember something about the guardian of the portal of fire from your\
+                  \nlifetime. Something specifically about speaking their name...', ['Say "Dionysus"', 'Say "Isis"', 'Say "Henet Requ"'],['ScenarioEnd', 'ScenarioEnd', 'Scenario9'],[[-1,0,0], [-1,0,0], [-1,0,0]],['fire.jpg','Henet.jpg']],
+    'Scenario5': ['As you approach the figure you recognize her as Taweret, the patron of childbirth\
+                  \n"Hello Taweret, I was following Anubis but seem to have lost my way, do you know the way \
                   \nto the Hall of Maat?" Her brow furrowed. "I do know the way, although it is very treacherous. \
                   \nYou must follow me."',
                  ['Follow her', 'Suspecting a trap, run away'], ['Scenario7', 'Scenario6'], [[-1,10,0], [-1,0,0], [-1,0,0]],['Cavern.jpg', 'taweret.jpg']],
     'Scenario6': ['You try to make it through crowd on your own. Eventually after fighting the current of \
-                  \nsouls you see a gate. There appear to be two options, a water and land route. \
+                  \nsouls you see a gate. There appear to be two options, a water and fire route. \
                   \nWhat do you do?',
-                 ['Approach the land route gate.','Approach the water route'], ['ScenarioEnd', 'ScenarioEnd'], [[0,0,0], [0,0,0], [0,0,0]],['Cavern.jpg']],
+                 ['Approach the fire route.','Approach the water route'], ['Scenario4', 'ScenarioEnd'], [[-1,0,0], [-1,0,0], [-1,0,0]],['Cavern.jpg']],
     'Scenario7': ['Congrats, through apotropaism you have befriended Taweret, the goddess of childbirth \
                   \nand fertility. By converting this god to your side, your strength has increased by 10! \
                   \n\nYou follow Taweret, who seems to easily navigate through the crowd of souls. Not wanting \
@@ -45,12 +51,49 @@ dialogue = {
                   \nWhich gate will you approach?',['Water Gate', 'Fire Gate'],['Scenario8','Scenario8'],[[-1,0,0], [-1,0,0], [-1,0,0]],['Cavern.jpg']],
     'Scenario8': ['Wait, before you go, remember that your journey to the Hall of Maat will be a dangerous one. \
                   \nNever trust anyone, and keep in mind that even if you make it to the Hall alive, your heart \
-                  \nwill be weighed, and you must must be seen as worthy to enter Sekhet-A’Aru, the field of reeds', ['Thank Taweret and Continue on', 'Thank Taweret and Continue On'],['ScenarioEnd','ScenarioEnd'],[[-1,0,0], [-1,0,0], [-1,0,0]],['Cavern.jpg', 'taweret.jpg']],
-    #'Scenario8': ['',[],[]]ing like
-    #'Scenario9': ['',[],[]]
+                  \nwill be weighed, and you must must be seen as worthy to enter Sekhet-A’Aru, the field of reeds', ['Thank Taweret and Approach the Water Gate', 'Thank Taweret and Approach the Fire Gate'],['ScenarioEnd','Scenario4'],[[-1,0,0], [-1,0,0], [-1,0,0]],['Cavern.jpg', 'taweret.jpg']],
+    'Scenario9': ['After you utter her name she weakens and huddles to the ground, brandishing her Khopesh \
+                  \nfor you to take. "You are a wise one, traveler." She says as she allows you to pass. \
+                  \nAs you wield the khopesh you feel strength rush through you.'
+                  , ['Continue On', 'Continue On'],['Scenario10', 'Scenario10'],[[-1,10,0], [-1,10,0], [-1,10,0]],['fire.jpg','Henet.jpg']],
+    'Scenario10': ['As you walk through the fiery cavern, you stumble upon a loaf of bread!- \
+                  \n-Well, toast, as it has been burned pretty badly. The bread is on a ledge, but out of the\
+                  \ncorner of your eye you spot a Medjed, a ghost-like demon, scrabbling \
+                  \nagainst the rock, attempting and failing to reach the bread. What do you do?'
+                  , ['Hand it the bread','Eat the bread to restore your health'],['Scenario11','Scenario11'],[[-1,0,1],[5,0,-1],[-1,0,0]],['fire.jpg', 'Henet.jpg']],
+    'Scenario11': ['After passing by the MedJed you follow the path for a while. You eventually come to a door. \
+                  \nYou can hear some scrabbles and squeals and other curious noises coming from the other side. \
+                  \nWhat do you do?'
+                  , ['Listen at the keyhole', 'Run in and fight no matter what','Try and find a way around'],['Scenario12','ScenarioFight','Scenario14','Scenario13'],[[-1,0,-1],[-1,0,1],[-1,0,1]],['fire.jpg']],
+    'Scenario12': ['You approach the keyhole, and put your eye down to it, listening the whole\
+                  \ntime to try and make out any intelligible noises. As you peer through the\
+                  \nsmall hole, the door opens from the other side and you are surrounded by sundisks.'
+                  , ['Fight them off','Yell "Sundisks, servants of Ra, cease!"','Dance off (come on this time it might work)'],['ScenarioFight','ScenarioEnd','ScenarioEnd','Scenario13'],[[-1,0,0],[-1,0,0],[-1,0,0]],['fire.jpg']],
+    'Scenario13': ['The dust clears, and you stand over several piles of dazed sundisks. \
+                  \n One of them appears to have dropped a bag of grain. What do you do?'
+                  ,['Take the bag of grain','Ignore the grain and continue on your way'],['Scenario14','Scenario14',],[[3,0,-1],[-1,0,1],[-1,0,0]],['fire.jpg']],
+    'Scenario14': ['You stumble out into a new kind of light, it appears that you made it out of the cave.\
+                  \n You approach a huge set of gates, guarded by a massive demon known as "apep"'
+                  ,['Fight it','Fight it','Fight it'],['ScenarioFight','ScenarioFight','ScenarioFight','Scenario16'],[[-1,0,0],[-1,0,0],[-1,0,0]],['fire.jpg']],
+    'Scenario15': ['Apep appears unimpressed.'
+                  ,['Fight it Again','Fight it Again','Fight it Again'],['ScenarioFight','ScenarioFight','ScenarioFight','Scenario16'],[[-1,0,0],[-1,0,0],[-1,0,0]],['fire.jpg']],
+    'Scenario16': ['Apep is bewildered at your strength. One more time might do it!'
+                  ,['Fight it Again','Fight it Again','Fight it Again'],['ScenarioFight','ScenarioFight','ScenarioFight','ScenarioWeigh'],[[-1,0,0],[-1,0,0],[-1,0,0]],['fire.jpg']],
+    'ScenarioWeigh': ['You enter into the huge doors of what is clearly the hall of two truths. \
+                  \n In a throne sits Anubis, and at his feet sits Ammit the devourer. A single pair of\
+                  \n scales lies ready to judge the heart of those brave enough to attempt. You see Anubis\
+                  \n pacing on one side of the hall, and he nods to you. Osiris beckons you towards the scales\
+                  \n and you stand ready to act. What will you do?'
+                  ,['Weigh your heart','Go out fighting'],['ScenarioWeigh','ScenarioEnd'],[[0,0,0],[0,0,0],[0,0,0]],['fire.jpg']],
+    'FinalScenario': ['Congratulations! You made it. You step out of the hall and onto a small \
+                  \n boat, piloted by hraf-hef, the ferryman. He smiles at you, and offers you his congratulations.'
+                  ,['Smile back','ignore him','say thank you'],['ScenarioWin','ScenarioEnd','ScenarioWin'],[[-1,0,0],[-1,0,0],[-1,0,0]],['fire.jpg']],
+    # 'Scenario17': ['Apep is bewildered at your strength. One more time might do it!'
+    #               ,['Fight it Again','Fight it Again','Fight it Again'],['ScenarioFight','ScenarioFight','ScenarioFight','ScenarioEnd'],[[-1,0,0],[-1,0,0],[-1,0,0]],['fire.jpg']],
     #'Scenario10': ['',[],[]]
-    #'Scenario11': ['',[],[]]
-    'ScenarioEnd': ['k', ['Play Again', 'Play Again'], ['Scenario1', 'Scenario1'],[[0,0,0], [0,0,0], [0,0,0]],['Youlost.jpg']]
+    #'Scenario11': ['',[],[]]tail of a crocodile
+    'ScenarioWin': ['', ['Play Again', 'Play Again'], ['Scenario1', 'Scenario1'],[[0,0,0], [0,0,0], [0,0,0]],['Youlost.jpg']],
+    'ScenarioEnd': ['', ['Play Again', 'Play Again'], ['Scenario1', 'Scenario1'],[[0,0,0], [0,0,0], [0,0,0]],['Youlost.jpg']]
     }
 
 file_path1 = '/home/cmay/Documents/OrganTrail/'
@@ -85,13 +128,20 @@ def fight(strength, health, currentScenario, lastScenario):
         print('Sorry, you lost.\n')
     return health, currentScenario
 
+def merit_check(merit, currentScenario):
+    if merit >= 0:
+        currentScenario = 'FinalScenario'
+    else:
+        currentScenario = 'ScenarioEnd'
+    return currentScenario
+
 
 def multiline_render(text, x, y, font):
     lines = text.splitlines()
     for i,l in enumerate(lines):
         text = font.render(l, 0, black)
-        if text.get_size()[0] > 1100:
-            temp_surface = pygame.Surface((1100, 26))
+        if text.get_size()[0] > 1115:
+            temp_surface = pygame.Surface((1115, 26))
         else:
             temp_surface = pygame.Surface(text.get_size())
         if text.get_size() != (1, 25):
@@ -144,7 +194,7 @@ def visualize(currentScenario, health, strength, additionalString):
         if len(option3) > 10:
             multiline_render(option3, 680-len(option3)*5, 660, font)
         else:
-            multiline_render(option3, 680, 660, font)
+            multiline_render(option3, 680-len(option3)*5, 660, font)
 
 
 pygame.init()
@@ -220,6 +270,8 @@ while dead == False:
             currentScenario = dialogue[currentScenario][2][int(Input) - 1]
             if currentScenario == 'ScenarioFight':
                 [health, currentScenario] = fight(strength, health, currentScenario, lastScenario)
+            if currentScenario == 'ScenarioWeigh':
+                merit_check(merit, currentScenario)
 
 
             scenarioCount+=1
